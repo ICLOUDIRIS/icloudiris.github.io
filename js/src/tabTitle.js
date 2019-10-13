ca65d9b53d625d0ff409ea5e40a8d83bb318c375
lcoudiris <hey@lcjh.com>
2019-10-13 19:40:08 +0800

    Site updated: 2019-10-13 19:40:02

--- js/src/tabTitle.js
+++ js/src/tabTitle.js
@@ -1,1 +1,16 @@
-"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/TEP.ico"),document.title=" ( ఠൠఠ )ﾉ隐身",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = ' ( ఠൠఠ )ﾉ隐身' ;
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
